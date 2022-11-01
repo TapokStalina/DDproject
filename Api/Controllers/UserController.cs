4382ca9c3332426b29f83801bec35917fe4d3af7
Anton_Smirnov <tapokstalina88@gmail.com>
2022-11-01 11:05:02 +0300

    Add project files.

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Models;
+using Api.Services;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserServices _userServices;  
+
+        public UserController(UserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+        [HttpPost]
+        public async Task CreateUser(CreateUserModel model) => await _userServices.CreateUser(model);
+
+
+        [HttpGet]
+       public async Task<List<UserModel>> GetUsers() => await _userServices.GetUsers();
+        
+
+    }
+}
---
+++ Api/Services/UserServices.cs
@@ -0,0 +1,33 @@
+﻿using Api.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Api.Services
+{
+    public class UserServices
+    {
+        private readonly IMapper _mapper;
+        private readonly DAL.DataContext _context;
+
+        public UserServices(IMapper mapper, DataContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task CreateUser(CreateUserModel model)
+        {
+            var dbUser = _mapper.Map<DAL.Entities.User>(model);
+            await _context.Users.AddAsync(dbUser);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<UserModel>> GetUsers()
+        {
+            return await _context.Users.AsNoTracking().ProjectTo<UserModel>(_mapper.ConfigurationProvider).ToListAsync();
+
+        }
+    }
+}
